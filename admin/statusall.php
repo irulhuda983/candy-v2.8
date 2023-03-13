3a3dbfd69aecf6acab0fc286a209d70abf3b0948
Irul Huda <irulhuda983@gmail.com>
2023-03-13 09:24:21 +0700

    fix status peserta

--- admin/statusall.php
+++ admin/statusall.php
@@ -10,15 +10,15 @@ include "../config/config.default.php";
 include "../config/config.function.php";
 $pengawas = mysqli_fetch_array(mysqli_query($koneksi, "SELECT * FROM pengawas  WHERE id_pengawas='$_SESSION[id_pengawas]'"));
 if ($pengawas['level'] == 'admin') {
-    $nilaiq = mysqli_query($koneksi, "SELECT *  FROM nilai s LEFT JOIN ujian c ON s.id_ujian=c.id_ujian  where c.status='1' and s.id_siswa<>'' GROUP by s.id_nilai DESC");
+    $nilaiq = mysqli_query($koneksi, "SELECT *  FROM nilai s LEFT JOIN ujian c ON s.id_ujian=c.id_ujian  where c.status='1' and s.id_siswa<>'' GROUP by s.id_nilai ORDER BY s.id_nilai DESC");
 } else {
-    $nilaiq = mysqli_query($koneksi, "SELECT *  FROM nilai s LEFT JOIN ujian c ON s.id_ujian=c.id_ujian  where c.status='1' and s.id_siswa<>'' and c.id_guru='$_SESSION[id_pengawas]' GROUP by s.id_nilai DESC");
+    $nilaiq = mysqli_query($koneksi, "SELECT *  FROM nilai s LEFT JOIN ujian c ON s.id_ujian=c.id_ujian  where c.status='1' and s.id_siswa<>'' and c.id_guru='$_SESSION[id_pengawas]' GROUP by s.id_nilai ORDER BY s.id_nilai DESC");
 }
 while ($nilai = mysqli_fetch_array($nilaiq)) {
     $tglx = strtotime($nilai['ujian_mulai']);
     $tgl = date('Y-m-d', $tglx);
     $tglsekarang = date('Y-m-d');
-    echo "<tr><td>".($tgl == $tglsekarang)."</td></tr>";
+    echo "<tr><td id='tanggal'>".($tgl == $tglsekarang)."</td></tr>";
     if ($tgl == $tglsekarang) {
         $no++;
         $ket = '';
